af633481cf93981fd3c4fa97273c5e50d8071df4
omar212 <omarelnagdy16@gmail.com>
2021-10-19 16:36:53 -0400

    progress

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -9,6 +9,24 @@ import ResumeItem from '../Components/ResumeItem'
 function Resume() {
     const briefcase  = <BusinessCenterIcon/>
     const school = <SchoolIcon/>
+    const internDescr = "At IBM, I worked and collaborated with a diverse team of developers," 
+    + "UX/UI designers, PO's, IM's, testers, etc. from around the world in order to transition " 
+    + " IBM’s global \ ledger application for all journal activity to a cloud-native solution utilizing React for the " 
+    + "front-end, and a variety of technologies for the back-end micro-services, such as SQL, Java Spring, JDBC, " 
+    + "k8s for container-orchestration, and Docker for containerization. As a team, we additionally leveraged a " + 
+    "host of CI/CD software to develop the application along with Git and GitHub for version control, and Travis for integrated build tests."
+
+    const coopDescr = "Continued the work I finished as an intern, but now as a Co-Op while I was a student at City College of New York. "
+    + " I participated in weekly stand-up meetings relating to the JIRA stories I was assigned. With each story, I assisted the team remotely " 
+    + " with various bug fixes and quality assurance testing of the front-end and back-end of the global ledger application."
+
+    const fullTimeDesc = "Full-stack software engineer focused primarily on the front-end and back-end of the " 
+    + "IBM cloud-native applications GLUI, the single-entry point to IBM’s ledger for all global journal activity and GUDA, " 
+    + "the global application for all IBM departmental activities. Works with testers, PO's, IM's, UI/UX designers, and users " 
+    + "to enhance IBM's cloud-native journaling and departmental software. Leverages modern front-end technologies, such as React and Angular, " 
+    + " and modern back-end technologies such as Node.js and Express, Java RESTful services using Spring, JDBC, and SQL. Utilizes the IBM DB2 and PostgreSQL " 
+    + "relational databases, Docker for containerization, Artifactory for encapsulating all Docker images, and IBM Hybrid Cloud." 
+    
     
     return (
         <MainLayout>
@@ -16,10 +34,39 @@ function Resume() {
                 <Title title={'Resume'} span={'resume'} />
                 <InnerLayout>
                     <div className="small-title">
+                        <SmallTitle icon={school} title={'Education'}/>
+                    </div>
+                    <div className="resume-content">
+                        <ResumeItem 
+                            year={'Jan ‘15 - Dec ‘19'}
+                            title={'The City College of New York, CUNY, New York, NY'}
+                            subTitle={'Bachelor of Science in Computer Science'}
+                            text={'Grove School of Engineering \n Minor: Mathematics \ GPA: 3.0'}
+                        />
+                    </div>
+                    <div className="small-title">
                         <SmallTitle icon={briefcase} title={'Working Experience'}/>
                     </div>
                     <div className="resume-content">
                         <ResumeItem 
+                            year={'May ‘19 - Aug ‘19'}
+                            title={'IBM'}
+                            subTitle={'Software Engineering Intern'}
+                            text={internDescr}
+                        />
+                         <ResumeItem 
+                            year={'Aug ‘19 - Jun ‘20'}
+                            title={'IBM'}
+                            subTitle={'Software Engineering Co-op'}
+                            text={coopDescr}
+                        />
+                         <ResumeItem 
+                            year={'Jan ‘20 - Present'}
+                            title={'IBM'}
+                            subTitle={'Software Engineer'}
+                            text={fullTimeDesc}
+                        />
+                         <ResumeItem 
                             year={'2015 - 2019'}
                             title={'Bachelors in Computer Science and Minor in Mathematics'}
                             subTitle={'City College of New York'}
--- src/Components/ResumeItem.js
+++ src/Components/ResumeItem.js
@@ -19,15 +19,49 @@ function ResumeItem({year, title, subTitle, text }) {
 
 const ResumeItemStyled = styled.div`
     display: flex;
+    width: 100%;
+    border: 1px solid purple;
+    
+    &:not(:last-child) {
+        padding-bottom: 3rem;
+    }
     .left-content {
-        width: 10%;
+        width: fit-content;
+        word-break: none;
+        padding-left: 40px;
+        position: relative;
+        border: 1px solid green;
+        &::before {
+            content: "";
+            position: absolute;
+            left: 0;
+            top: 5px;
+            width: 1rem;
+            height: 17px;
+            border-radius: 50%;
+            border: 2px solid var(--border-color);
+            background-color: var(--background-color-dark)
+        }
         p {
             display: inline-block; 
+            width: max-content;
+            border:1px solid red;
         }
 
     }
     .right-content {
-        margin-left:0%;
+        margin-left:6rem;
+        position: relative;
+        border: 1px solid green;
+        &::before {
+            content: "";
+            position: absolute;
+            left: -100px;
+            top: 15px;
+            width: 3rem;
+            height: 2px;
+            background-color: var(--border-color);
+        }
         h5 {
             color: var(--primary-color);
             font-size: 2.3rem;
@@ -36,6 +70,12 @@ const ResumeItemStyled = styled.div`
         h6 {
             padding-bottom: .4rem;
             font-size: 1.5rem;
+        }
+        p {
+            display: inline-block; 
+            word-break: break-word;
+            width: 50%;
+            border:1px solid red;
         }
     }
 `
