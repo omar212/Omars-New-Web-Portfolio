3899ef4863edab4eb03ad913667b2b4f37a0ab8c
omar212 <omarelnagdy16@gmail.com>
2021-10-19 17:06:21 -0400

    Updates

---
+++ precache-manifest.e6164c6a781d051c3d4916df99b68a67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8761fa702a29d1453a5",
+    "url": "/Omars-New-Web-Portfolio/static/css/main.d915cd2f.chunk.css"
+  },
+  {
+    "revision": "b8761fa702a29d1453a5",
+    "url": "/Omars-New-Web-Portfolio/static/js/main.e87161dc.chunk.js"
+  },
+  {
+    "revision": "fbf630eebc7a0a466eaa",
+    "url": "/Omars-New-Web-Portfolio/static/js/runtime~main.5cbc81c0.js"
+  },
+  {
+    "revision": "373193d08f73eb744458",
+    "url": "/Omars-New-Web-Portfolio/static/js/2.0dc1d360.chunk.js"
+  },
+  {
+    "revision": "db8e46895e8968a22bb6",
+    "url": "/Omars-New-Web-Portfolio/static/js/3.2aa1e6d9.chunk.js"
+  },
+  {
+    "revision": "ba81a4dd6ef731513fa05701bbdf584a",
+    "url": "/Omars-New-Web-Portfolio/static/media/blog1.ba81a4dd.jpg"
+  },
+  {
+    "revision": "26ec9f856f0a9a10f23e0ad08c1b3a1b",
+    "url": "/Omars-New-Web-Portfolio/static/media/blog2.26ec9f85.jpg"
+  },
+  {
+    "revision": "5323c60625808777afbeda5d3a943832",
+    "url": "/Omars-New-Web-Portfolio/static/media/avatar.5323c606.jpg"
+  },
+  {
+    "revision": "14dbe5a4d5669db0c78bcbccfe3a3bfe",
+    "url": "/Omars-New-Web-Portfolio/static/media/ai.14dbe5a4.jpg"
+  },
+  {
+    "revision": "0aea0f8a6df77d265ee87f92b5b383e9",
+    "url": "/Omars-New-Web-Portfolio/index.html"
+  }
+];
